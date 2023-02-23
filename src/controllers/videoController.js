193b73a64d92097bbc87cac94b06369643f57a4f
jaeiko <jkimkr08@daum.net>
2023-02-23 11:46:04 +0900

    feat, refactor: Add fake DB, change some variables

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,39 @@
-export const trending = (req, res) => res.send("Home page Video");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send("Watch Video");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 23,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload video");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
