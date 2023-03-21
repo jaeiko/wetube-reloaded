de17decfaa2abbbb4b22432fa1fe97871a5b980b
jaeiko <jkimkr08@daum.net>
2023-03-21 23:46:03 +0900

    feat : Router setting

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,35 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  getEdit,
+  postEdit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+  getChangePassword,
+  postChangePassword,
+} from "../controllers/userController";
+import {
+  avatarUpload,
+  protectorMiddleware,
+  publicOnlyMiddleware,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
-userRouter.get(":id", see);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
+userRouter.all(publicOnlyMiddleware).get("/github/start", startGithubLogin);
+userRouter.all(publicOnlyMiddleware).get("/github/finish", finishGithubLogin);
+userRouter.get("/:id", see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,14 +2,29 @@ import express from "express";
 import {
   upload,
   watch,
-  edit,
+  getEdit,
   deleteVideo,
+  postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
+import { protectorMiddleware, videoUpload } from "../middlewares";
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(videoUpload.fields([{ name: "video" }, { name: "thumb" }]), postUpload);
 
 export default videoRouter;
