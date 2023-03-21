79724a91001fc8f080746efad46f63957fbe1d5d
jaeiko <jkimkr08@daum.net>
2023-03-21 23:51:21 +0900

    feat : Set up sessions, MongoDb, and some routers

--- src/server.js
+++ src/server.js
@@ -1,21 +1,48 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import flash from "express-flash";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = 4000;
+import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
 
 const app = express(); // 변수명 상관없음, express application 생성(express 규칙)
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(logger);
-app.use("/", globalRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.get("/add-one", (req, res, next) => {
+  return res.send(`${req.session.id} ${req.session.potato}`);
+});
+
+app.use(flash());
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
+app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/api", apiRouter);
 
-const handleListening = () => console.log(`Server listening on port ${4000}`);
-
-app.listen(PORT, handleListening);
+export default app;
