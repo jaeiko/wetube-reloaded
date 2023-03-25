32ff316dbbf371a73b5388cc62b16e1c8695347d
jaeiko <jkimkr08@daum.net>
2023-03-25 23:59:47 +0900

    fix: Edit Avatar URL

--- src/middlewares.js
+++ src/middlewares.js
@@ -19,7 +19,6 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
-  console.log("세션:", req.session);
   next();
 };
 
