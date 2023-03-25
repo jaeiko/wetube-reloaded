1bac8e75d9422e77f44edde645366117204575db
jaeiko <jkimkr08@daum.net>
2023-03-26 00:04:32 +0900

    Revert "fix: Edit Avatar URL"
    This reverts commit 32ff316dbbf371a73b5388cc62b16e1c8695347d.

--- src/middlewares.js
+++ src/middlewares.js
@@ -19,6 +19,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  console.log("세션:", req.session);
   next();
 };
 
