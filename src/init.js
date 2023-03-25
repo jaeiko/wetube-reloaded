3f967dcfe79f43501a1590a9bd13f664de461ca7
jaeiko <jkimkr08@daum.net>
2023-03-25 20:57:13 +0900

    refactor : Change port output in console log

--- src/init.js
+++ src/init.js
@@ -8,6 +8,6 @@ import app from "./server";
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`✅Server listening on port ${4000}`);
+const handleListening = () => console.log(`✅Server listening on port ${PORT}`);
 
 app.listen(PORT, handleListening);
